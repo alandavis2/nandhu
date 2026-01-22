62b748535c5fda854953c0654d87c879663114e1
alandavis2 <alandavisck@gmail.com>
2026-01-22 16:13:08 +0530

    update code

--- front-end/src/components/ContactSection.tsx
+++ front-end/src/components/ContactSection.tsx
@@ -14,7 +14,7 @@ const ContactSection = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch("http://localhost:5001/api/send-email", {
+      const response = await fetch("https://nandhu-16sy.onrender.com/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
